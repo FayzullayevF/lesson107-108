8b12468edf66749f2b57106d8bcce79b02c3e231
FayzullayevF <fatxullofayzullayev6@gmail.com>
2024-12-26 14:25:05 +0500

    salom

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,21 @@ import 'package:lesson107/presentation/ScaffoldPart.dart';
 
 
 
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: HomePage(),
-//     );
-//   }
-// }
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
--- lib/presentation/RecentlyAdded.dart
+++ lib/presentation/RecentlyAdded.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lesson107/utils/constants.dart';
 
 class RecentItem extends StatelessWidget {
@@ -6,34 +7,187 @@ class RecentItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return  Row(
       children: [
-        Padding(
-            padding: EdgeInsets.only(left: 18),
-        child:
-        Text(
-          "Recently Added",
-          style: TextStyle(
-            color: AppColors.redPinkMain,
-            fontSize: 15,
-          ),
-        ),
-        ),
-        SizedBox(height: 10,),
-        Padding(
-          padding: EdgeInsets.only(left: 18),
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(14)
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 18),
+              child:
+              Text(
+                "Recently Added",
+                style: TextStyle(
+                  color: AppColors.redPinkMain,
+                  fontSize: 15,
                 ),
-                child: Image(image: AssetImage("assets/lemonade.png"),),
               ),
-            ],
-          ),
-        )
+            ),
+            SizedBox(height: 10,),
+            Padding(
+              padding: EdgeInsets.only(left: 18),
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(14)
+                    ),
+                    child: Image(
+                      image: AssetImage("assets/lemonade.png",),
+                      width: 165,
+                      height: 153,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -60,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      padding:  EdgeInsets.all(12),
+                      width: 165,
+                      height: 76,
+                      decoration:  BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(18),
+                          bottomRight: Radius.circular(18),
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Lemonade',
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: AppColors.backgound,
+                            ),
+                          ),
+                          Text("Acidic and refreshing",
+                            style: TextStyle(
+                                color: Color(0xFF3E2823),
+                                fontSize: 10
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("4",
+                                style: TextStyle(
+                                    color: AppColors.pinkSub,
+                                    fontSize: 12
+                                ),
+                              ),
+                              SizedBox(width: 3,),
+                              SvgPicture.asset("assets/icons/yulduz.svg"),
+                              SizedBox(width: 40,),
+                              SvgPicture.asset("assets/icons/oclock.svg",
+                                colorFilter: ColorFilter.mode(Color(0XFFEC888D), BlendMode.srcIn),
+                              ),
+                              SizedBox(width: 3,),
+                              Text("30min",
+                                style: TextStyle(
+                                    color: Color(0XFFEC888D),
+                                    fontSize: 10
+                                ),)
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 18),
+
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(14)),
+                    child: Image(
+                      image: AssetImage("assets/lemonade1.png",),
+                      width: 165,
+                      height: 153,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -60,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      padding:  EdgeInsets.all(12),
+                      width: 165,
+                      height: 76,
+                      decoration:  BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(18),
+                          bottomRight: Radius.circular(18),
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Lemonade',
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: AppColors.backgound,
+                            ),
+                          ),
+                          Text("Acidic and refreshing",
+                            style: TextStyle(
+                                color: Color(0xFF3E2823),
+                                fontSize: 10
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text("4",
+                                style: TextStyle(
+                                    color: AppColors.pinkSub,
+                                    fontSize: 12
+                                ),
+                              ),
+                              SizedBox(width: 3,),
+                              SvgPicture.asset("assets/icons/yulduz.svg"),
+                              SizedBox(width: 40,),
+                              SvgPicture.asset("assets/icons/oclock.svg",
+                                colorFilter: ColorFilter.mode(Color(0XFFEC888D), BlendMode.srcIn),
+                              ),
+                              SizedBox(width: 3,),
+                              Text("30min",
+                                style: TextStyle(
+                                    color: Color(0XFFEC888D),
+                                    fontSize: 10
+                                ),)
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ],
     );
   }
